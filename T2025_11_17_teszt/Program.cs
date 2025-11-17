fdb68958bd3b4862b48738c171b9f27036faa90a
levihsg <levilucy2@gmail.com>
2025-11-17 13:21:36 +0100

    asdasd

--- T2025_11_17_teszt/Program.cs
+++ T2025_11_17_teszt/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace T2025_11_17_teszt
+namespace _2025._11._17_teszt
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Másodfokú egyenlet megoldó program");
+            Console.Write("Adja meg az 'a' együtthatót: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg a 'b' együtthatót: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg a 'c' együtthatót: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = b * b - 4 * a * c;
+            if (d < 0)
+            {
+                Console.WriteLine("Nincs valós megoldás.");
+            }
+            else if (d == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"Egy valós megoldás van: x = {x}");
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine($"Két valós megoldás van: x1 = {x1}, x2 = {x2}");
+            }
         }
     }
 }
